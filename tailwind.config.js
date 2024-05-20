8e1d514fcfcaf191a1a30514141f567b6c67d66a
Manish Bhusal <manishbhusal6@gmail.com>
2024-05-20 18:17:20 +0545

    complete Booking Options section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'primary': '#0F172A',
         'secondary': '#64748B',
         'yellow-white': '#F6EEC9',
-        'green': '#0A6847',
+        'greeen': '#0A6847',
       },
       fontFamily: {
         'lato': ['Lato', 'sans-serif'],
