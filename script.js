fcaa144966cdcbfcd3b70dab6c1d093db45ee654
Manish Bhusal <manishbhusal6@gmail.com>
2024-05-21 20:50:27 +0545

    add animation & connect navbar with their respective id section

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const navDialog = document.getElementById("nav-dialog");
+const loginBtn = document.getElementById("login-btn");
+const talkToUs = document.getElementById("talktous-btn");
+const knowMore = document.getElementById("know-more");
+
 
 function handleMenu() {
   navDialog.classList.toggle("hidden");
 }
+// document.addEventListener("DOMContentLoaded", () => {
+
+  if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      if (email === "" || password === "") {
+        alert("Please fill in all fields");
+      } else {
+        alert("Login successful");
+      }
+    });
+  }
+
+  talkToUs.addEventListener("click", () => {
+    window.location.href = "book-consultation.html";
+  });
+
+  knowMore.addEventListener("click", () => {
+    window.location.href="#about-us";
+  })
+// });
